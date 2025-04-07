83dffdf8247e01eb8135fa14586e959755ce929f
Abimbola-star <aroyewunbimbola@gmail.com>
2025-04-07 03:08:23 -0500

    bug

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')  // AWS credentials
         EC2_INSTANCE_CREDENTIALS = credentials('ec2-instance')  // The SSH private key
-        AWS_DEFAULT_REGION: 'us-east-1'
+        AWS_DEFAULT_REGION: ('us-east-1')
     }
 
     parameters {
