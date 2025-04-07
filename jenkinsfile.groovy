c78a30bc6085114dac8707aad5312ed7436575d6
Abimbola-star <aroyewunbimbola@gmail.com>
2025-04-06 23:54:49 -0500

    bug

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,14 +2,11 @@ pipeline {
     agent any
 
     environment {
-        SCANNER_HOME = '/opt/sonar-scanner'  // Update with the correct path if needed
         VERSION = "${BUILD_NUMBER}"
         ARTIFACT_NAME = "angular-devops-app-artifact-${VERSION}.tar.gz"
         S3_BUCKET = 's3://ansible20-angular-app'
-        SONARQUBE_URL = "http://18.213.2.225:9000/"
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')  // You may need to add this if you're using AWS
-        PATH = "${PATH}:${SCANNER_HOME}/bin"  // Add SonarQube Scanner to PATH
     }
 
     parameters {
@@ -20,22 +17,6 @@ pipeline {
         stage('Checkout Code') {
             steps {
                 checkout scm
-            }
-        }
-
-        stage('SonarQube Analysis') {
-            steps {
-                withCredentials([string(credentialsId: 'SonarQube_Token', variable: 'SonarQube_Token')]) {
-                    withSonarQubeEnv('SonarQube_Token') {  // Ensure SonarQube is configured in Jenkins
-                        sh """
-                        sonar-scanner \
-                        -Dsonar.projectKey=angular-devops-app \
-                        -Dsonar.sources=. \
-                        -Dsonar.host.url=${SONARQUBE_URL} \
-                        -Dsonar.login=sqp_febce8fc5a1a953848d24f0d94279d85cc9ed4cd 
-                        """
-                    }
-                }
             }
         }
 
